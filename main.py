770e5e517a4e2ac1b9287178c3b4accecc17e4ae
Nicolai Weitkemper <nico.weio@gmail.com>
2020-03-10 21:32:55 +0100

    v4

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ ENDPOINT = 'https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush'
 
 
 def doRequest(args):
+    print(args.deviceNames)
     filteredParams = {k: v for k, v in vars(args).items() if v is not None}
+    if filteredParams['deviceNames']:
+        filteredParams['deviceNames'] = ','.join(filteredParams['deviceNames'])
     filteredParams['apikey'] = APIKEY
     url = makeRequestUrl(filteredParams)
+    print(url)
     req = urllib.request.urlopen(url)
     response = json.load(req)
     if (response['success']):
@@ -21,19 +25,16 @@ def doRequest(args):
 
 
 def makeRequestUrl(params):
-
     url_parts = list(urllib.parse.urlparse(ENDPOINT))
     query = dict(urllib.parse.parse_qsl(url_parts[4]))
     query.update(params)
-
     url_parts[4] = urllib.parse.urlencode(query)
-
     return urllib.parse.urlunparse(url_parts)
 
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 
-parser.add_argument("-n", "--device-names", dest="deviceNames", help="device names, separated by comma")
+parser.add_argument("-n", "--device-names", dest="deviceNames", nargs='*', help="device names, separated by comma")
 parser.add_argument("--callnumber", help="sets the callnumber")
 parser.add_argument("--clipboard", help="sets the clipboard")
 parser.add_argument("--text", help="sets the text")
@@ -42,5 +43,4 @@ parser.add_argument("--url", help="sets the url")
 
 args = parser.parse_args()
 
-# makeRequestUrl()
 doRequest(args)
